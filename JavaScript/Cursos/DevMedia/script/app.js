b294a0ca9c36c57879862ea2cd6f9ae243998f79
Mateus Marinho <mateus.marinho@hotmail.com.br>
2021-03-17 11:16:44 -0300

    notas

--- JavaScript/Cursos/DevMedia/script/app.js
+++ JavaScript/Cursos/DevMedia/script/app.js
@@ -520,3 +520,30 @@ while(index < cores.length) {
     index++;
 }
 */
+/* 
+let contador = 0;
+
+let notas = [5, 7, 6, 8];
+
+let soma = 0;
+let media = 0;
+
+do {
+
+    console.log(notas[contador]);
+
+    soma = soma + notas[contador];
+
+    contador++;
+
+} while ( contador < notas.length );
+
+media = soma / notas.length;
+
+if( media >= 6 ){
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
+ */
+
